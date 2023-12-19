5fdde90977d98b45f10bef5ef5f2008e809800f6
gilles20393 <gilles1996@outlook.com>
2023-12-19 23:56:33 +0100

    init

---
+++ src/main/java/be/thomasmore/bookserver/model/Author.java
@@ -0,0 +1,36 @@
+package be.thomasmore.bookserver.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(exclude = {"books"})
+@ToString(exclude = {"books"})
+@Entity
+public class Author {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+
+    @NotBlank(message = "Author name should not be blank")
+    @NotNull
+    private String name;
+
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
+    private Set<Book> books;
+
+    public Author(int id) {
+        this.id = id;
+    }
+
+
+
+}
---
+++ src/main/java/be/thomasmore/bookserver/model/Book.java
@@ -0,0 +1,39 @@
+package be.thomasmore.bookserver.model;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode(exclude = {"authors"})
+@ToString(exclude = {"authors"})
+@Entity
+public class Book {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+
+    @NotBlank(message = "Book Title should not be blank")
+    @NotNull
+    private String title;
+
+    @Min(value = 0, message = "price should not be smaller than 0")
+    @Max(value = 200, message = "price should not be greater than 200")
+    Integer priceInEur;
+
+    //todo: clean up (with flyway)
+    private String author = ""; //this is not normalized but I don't care for this example
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Author> authors;
+}
+
